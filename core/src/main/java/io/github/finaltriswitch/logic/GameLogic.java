4228938fde98fb9aab0d507f9c445e0abf1ea727
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-05-03 13:45:16 +0500

    add logic game is a test first v

--- core/src/main/java/io/github/finaltriswitch/logic/GameLogic.java
+++ core/src/main/java/io/github/finaltriswitch/logic/GameLogic.java
@@ -1,1 +1,28 @@
-.
+package io.github.finaltriswitch.logic;
+import io.github.finaltriswitch.characters.MissJ;
+import io.github.finaltriswitch.characters.MissK;
+import io.github.finaltriswitch.characters.MrB;
+public class GameLogic {
+    private MissJ missJ;
+    private MissK missK;
+    private MrB mrB;
+    public GameLogic() {
+        missJ = new MissJ();
+        missK = new MissK();
+        mrB = new MrB();
+    }
+    public MissJ getMissJ() {
+        return missJ;
+    }
+    public MissK getMissK() {
+        return missK;
+    }
+    public MrB getMrB() {
+        return mrB;
+    }
+    public void dispose() {
+        missJ.dispose();
+        missK.dispose();
+        mrB.dispose();
+    }
+}
