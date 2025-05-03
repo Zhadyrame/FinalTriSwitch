fefe20123e1e5a23d8ca86cdd9937c8a8321b9e7
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-05-03 13:55:25 +0500

    Add untils

--- core/src/main/java/io/github/finaltriswitch/untils/InputHandler.java
+++ core/src/main/java/io/github/finaltriswitch/untils/InputHandler.java
@@ -1,1 +1,6 @@
-.
+package io.github.finaltriswitch.untils;
+
+public class InputHandler {
+    // потом тут будет логика управления)
+
+}
--- core/src/main/java/io/github/finaltriswitch/untils/Utils.java
+++ core/src/main/java/io/github/finaltriswitch/untils/Utils.java
@@ -1,1 +1,7 @@
-.
+package io.github.finaltriswitch.untils;
+
+public class Utils {
+    public static float clamp(float value, float min, float max) {
+        return Math.max(min, Math.min(max, value));
+    }
+}
