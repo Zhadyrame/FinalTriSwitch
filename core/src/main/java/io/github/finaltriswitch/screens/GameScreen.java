eff8acae812815ddb38eeb339b0e38e81c10d222
Zhadyrame <zhadyra.nadirkhan@narxoz.kz>
2025-05-03 13:49:30 +0500

    Add screens

--- core/src/main/java/io/github/finaltriswitch/screens/GameScreen.java
+++ core/src/main/java/io/github/finaltriswitch/screens/GameScreen.java
@@ -1,1 +1,47 @@
-.
+package io.github.finaltriswitch.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import io.github.finaltriswitch.Main;
+import io.github.finaltriswitch.logic.GameLogic;
+import io.github.finaltriswitch.levels.Level1;
+
+public class GameScreen extends ScreenAdapter {
+    private Main game;
+    private GameLogic logic;
+    private Level1 level;
+
+    public GameScreen(Main game) {
+        this.game = game;
+        logic = new GameLogic();
+        level = new Level1();
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+
+        Gdx.gl.glClearColor(0.2f, 0.3f, 0.5f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        level.render(game.batch);
+        logic.getMissJ().render(game.batch);
+        logic.getMissK().render(game.batch);
+        logic.getMrB().render(game.batch);
+        game.batch.end();
+    }
+
+    private void update(float delta) {
+        logic.getMissJ().update(delta);
+        logic.getMissK().update(delta);
+        logic.getMrB().update(delta);
+    }
+
+    @Override
+    public void dispose() {
+        logic.dispose();
+        level.dispose();
+    }
+}
--- core/src/main/java/io/github/finaltriswitch/screens/MenuScreen.java
+++ core/src/main/java/io/github/finaltriswitch/screens/MenuScreen.java
@@ -1,1 +1,35 @@
-.
+package io.github.finaltriswitch.screens;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.github.finaltriswitch.Main;
+public class MenuScreen extends ScreenAdapter {
+    private Main game;
+    private BitmapFont font;
+
+    public MenuScreen(Main game) {
+        this.game = game;
+        font = new BitmapFont();
+    }
+
+    @Override
+    public void render(float delta) {
+        if (Gdx.input.justTouched()) {
+            game.setScreen(new GameScreen(game));
+        }
+
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+        font.draw(game.batch, "Touch to Start!", 300, 300);
+        game.batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        font.dispose();
+    }
+}
