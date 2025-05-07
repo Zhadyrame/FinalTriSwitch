ea479ccceb9ab3245129e2e83a75a50c1045f4f2
Qaraqat-ul <karakat.zhumagali@narxoz.kz>
2025-05-08 03:35:52 +0500

    Твой

--- core/src/main/java/io/github/finaltriswitch/levels/Level2.java
+++ core/src/main/java/io/github/finaltriswitch/levels/Level2.java
@@ -4,17 +4,73 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Level2 {
-    private Texture background;
+    private Texture levelTexture;
+    private Texture blockTexture;
+    private Texture doorTexture;
+
+    private float blockX;
+    private float blockY;
+    private float doorX;
+    private float doorY;
 
     public Level2() {
-        background = new Texture("level2.png");
+        levelTexture = new Texture("assets/level2.png");
+        blockTexture = new Texture("assets/metal_block.png");
+        doorTexture = new Texture("assets/door.png");
+
+        blockX = 900;
+        blockY = 200;
+
+        doorX = 1200;
+        doorY = 300;
     }
 
     public void render(SpriteBatch batch) {
-        batch.draw(background, 0, 0);
+        if (levelTexture != null) {
+            batch.draw(levelTexture, 0, 0, 1600, 1200);
+        }
+        if (blockTexture != null) {
+            batch.draw(blockTexture, blockX, blockY, blockTexture.getWidth() / 5f, blockTexture.getHeight() / 5f);
+        }
+        if (doorTexture != null) {
+            batch.draw(doorTexture, doorX, doorY, doorTexture.getWidth() / 4f, doorTexture.getHeight() / 4f);
+        }
+    }
+
+    public float getBlockX() {
+        return blockX;
+    }
+
+    public float getBlockY() {
+        return blockY;
+    }
+
+    public void moveBlock(float newX, float newY) {
+        if (newX >= 0 && newX <= 1500 - blockTexture.getWidth() / 2f) {
+            blockX = newX;
+        }
+        blockY = 200;
+    }
+
+    public float getDoorX() {
+        return doorX;
+    }
+
+    public float getDoorY() {
+        return doorY;
+    }
+
+    public boolean isAtDoor(float characterX, float characterY) {
+        float doorWidth = doorTexture.getWidth() / 4f;
+        float doorHeight = doorTexture.getHeight() / 4f;
+
+        return Math.abs(characterX - doorX) < doorWidth &&
+            Math.abs(characterY - doorY) < doorHeight;
     }
 
     public void dispose() {
-        background.dispose();
+        if (levelTexture != null) levelTexture.dispose();
+        if (blockTexture != null) blockTexture.dispose();
+        if (doorTexture != null) doorTexture.dispose();
     }
 }
